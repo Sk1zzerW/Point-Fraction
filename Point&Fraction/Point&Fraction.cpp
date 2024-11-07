03f63b4ee275c1f122d87995869c6735e074e180
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 21:37:09 +0200

    Добавьте файлы проекта.

---
+++ Point&Fraction/Point&Fraction.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+class Point {
+public:
+    int x, y;
+
+    Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+    Point operator+(const Point& other) const {
+        return Point(x + other.x, y + other.y);
+    }
+
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+
+    Point operator*(const int scalar) const {
+        return Point(x * scalar, y * scalar);
+    }
+
+    Point operator/(const int scalar) const {
+        return Point(x / scalar, y / scalar);
+    }
+
+    void print() const {
+        std::cout << "(" << x << ", " << y << ")" << std::endl;
+    }
+};
